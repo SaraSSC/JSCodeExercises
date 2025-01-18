f34191110c55635073baa75dfd52919ed77abbb8
Sattineez_SC <83277661+SaraSSC@users.noreply.github.com>
2025-01-18 13:39:22 +0000

    Finishing the easterEgg functionality and adding the option in the `kill monster` object

--- RolePlayGame/script.js
+++ RolePlayGame/script.js
@@ -69,7 +69,7 @@ const locations = [
     {
         name : "kill monster",
         "button text" : ["Go to town square", "Go to town square", "Go to town square"],
-        "button functions" : [goTown, goTown, goTown],
+        "button functions" : [goTown, goTown, easterEgg],
         text : "The monster screams 'Arg!' as it dies. You gain experience points and find gold."
     },
     {
@@ -304,7 +304,28 @@ function easterEgg(){
     update(locations[7]);
 }
 
-function pick(guess){}
+function pick(guess){
+    const numbers = [];
+
+    while (numbers.length < 10){
+        numbers.push(Math.floor(Math.random() * 11));
+        text.innerText = `You picked ${guess}. Here are the random numbers:\n`;
+        for (i = 0; i < 10; i++){
+            // text.innerText += numbers[i] + "\n";
+            text.innerText += `${numbers[i]}\n`;
+        };
+
+        if (numbers.includes(guess)){
+            text.innerText += `Right! You win 20 gold!`;
+            goldText.innerText = gold += 20;
+        } else {
+            text.innerText += `Wrong! You lose 10 health!`;
+            healthText.innerText = health -= 10;
+             if (health <= 0){
+                lose();
+        };
+    }
+}
 
 function pickTwo(){
     pick(2);
